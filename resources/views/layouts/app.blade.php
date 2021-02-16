18ffcbb590fb1f7b2bebd351bb8452419b99cbe4
John <faramere07@gmail.com>
2021-02-16 11:49:37 -0800

    'new'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 </head>
 <body class="bg-gray-100">
-	<nav class="p-6 bg-white flex justify-between">
+	<nav class="p-6 bg-white flex justify-between mb-6">
 		<ul class="flex items-center">
 			<li>
 				<a href="" class="p-4 text-lg">Home</a>
@@ -16,6 +16,21 @@
 			</li>
 			<li>
 				<a href="" class="p-4 text-lg">Post</a>
+			</li>
+		</ul>
+
+		<ul class="flex items-center">
+			<li>
+				<a href="" class="p-4 text-lg">Name</a>
+			</li>
+			<li>
+				<a href="" class="p-4 text-lg">Login</a>
+			</li>
+			<li>
+				<a href="" class="p-4 text-lg">Register</a>
+			</li>
+			<li>
+				<a href="" class="p-4 text-lg">Logout</a>
 			</li>
 		</ul>
 
