0bea94b3e8e5104e9f7492d49558fc4d955f63d0
John <faramere07@gmail.com>
2021-02-16 11:40:37 -0800

    'message'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,8 +2,24 @@
 <html>
 <head>
 	<title>Laravel Learn</title>
+
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 </head>
-<body>
+<body class="bg-gray-100">
+	<nav class="p-6 bg-white flex justify-between">
+		<ul class="flex items-center">
+			<li>
+				<a href="" class="p-4 text-lg">Home</a>
+			</li>
+			<li>
+				<a href="" class="p-4 text-lg">Dashboard</a>
+			</li>
+			<li>
+				<a href="" class="p-4 text-lg">Post</a>
+			</li>
+		</ul>
+
+	</nav>
 	@yield('content')
 </body>
 </html>
